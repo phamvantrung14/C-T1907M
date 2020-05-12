224dcc82fde1a06ee12fe3fe80611ee745f3041d
trung <phamvantrung14111994@gmail.com>
2020-05-12 10:16:49 +0700

    s
    ok

---
+++ Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lion lion = new Lion();
+            Tiger tiger = new Tiger();
+            
+            tiger.SetMe("tiger",100);
+            lion.SetMe("lion",200);
+            
+            tiger.Show();
+            lion.Show();
+        }
+    }
+}
---
+++ GeometryExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace GeometryExample
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double radius;
+            double height;
+            Console.WriteLine("Enter the dimenstions of the cylinder");
+            Console.WriteLine("Radius: "); 
+            radius = double.Parse( Console.ReadLine());
+            Console.WriteLine("Height: ");
+            height = double.Parse(Console.ReadLine());
+            
+            Cylinder c = new Cylinder(radius,height);
+            c.Process();
+            c.Result();
+        }
+        
+        
+    }
+}
